219640937c4c934a26a7ee1b2ecff48235cb0ad8
MD PARVEJ <mparvejind@gmail.com>
2024-12-11 01:12:39 +0530

    upd

---
+++ Strings & Conditional Statements/prac.py
@@ -0,0 +1,5 @@
+name = input("Enter Your Name - ")
+print("Length of Your name is", len(name))
+
+str = "Hi, $Iam the $ symbol $99.99"
+print(str.count("$"))
--- Strings & Conditional Statements/str_function.py
+++ Strings & Conditional Statements/str_function.py
@@ -2,8 +2,17 @@ str = "I am studying python from ApnaCollege"
 print(str.endswith("ege")) # True
 print(str.endswith("apna")) # False
 
-str1 = "i am studying python from ApnaCollege"
+str1 = "i am studying Data Science from IITM"
 print(str1.capitalize())
         # OR
 str1 = str1.capitalize()
 print(str1)
+
+str2 = "I am studying Quantitative Finance from Soas University of London"
+print(str2.replace("Finance","Economics"))
+
+str3 = "I am studying Quant from World Quant University"
+print(str3.find("World"))
+
+str4 = "I am doing Bachelor's from IITM"
+print(str.count("from"))
